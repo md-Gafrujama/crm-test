c405a6497590a4ba403188e2ccfdb312dc4b74a9
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-08-12 23:25:12 +0530

    c

--- server-crm/middleware/cors.middleware.js
+++ server-crm/middleware/cors.middleware.js
@@ -1,24 +1,34 @@
-const allowedOrigins = [
-  'https://crm-test-eyrb.vercel.app',
-  'http://localhost:5173',
+// const allowedOrigins = [
+//   'https://crm-test-eyrb.vercel.app',
+//   'http://localhost:5173',
  
-];
+// ];
 
-const corsMiddleware = (req, res, next) => {
-  const origin = req.headers.origin;
+// const corsMiddleware = (req, res, next) => {
+//   const origin = req.headers.origin;
   
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+//   if (allowedOrigins.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
   
-  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Credentials', 'true');
   
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
   
-  next();
-};
-cors.middleware.js
+//   next();
+// };
+// cors.middleware.js
+import cors from 'cors';
+
+const corsMiddleware = cors({
+  origin: ['https://crm-test-eyrb.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
+export default corsMiddleware;
