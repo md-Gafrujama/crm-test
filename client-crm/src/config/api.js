16960c06962dbe063972c296aeec25c89132946b
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-08-13 13:44:31 +0530

    commit

--- client-crm/src/config/api.js
+++ client-crm/src/config/api.js
@@ -4,7 +4,7 @@
 // // Get API base URL from environment variables
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8888';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://crm-test-green.vercel.app';
-// export { API_BASE_URL };
+export { API_BASE_URL };
 
 
 //const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8888';
