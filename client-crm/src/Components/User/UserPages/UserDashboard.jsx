82552a9c3c4c2e1d2ff249446873d8cbb8631fc2
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-08-28 12:29:45 +0530

    cc

--- client-crm/src/Components/User/UserPages/UserDashboard.jsx
+++ client-crm/src/Components/User/UserPages/UserDashboard.jsx
@@ -431,7 +431,22 @@ const UserDashboard = ({ onLogout }) => {
                   </p>
                 </div>
                 <div>
-               
+                  <button className="flex items-center gap-2 px-4 py-2 bg-white p-5 justify-center hover:bg-gray-100 text-[#ff8633] rounded-md transition-colors shadow-md">
+                    Upload Data
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                        transform="rotate(180 10 10)"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
               {/* Enhanced Header */}
