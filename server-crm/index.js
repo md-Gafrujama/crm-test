b12099987c109d3d9c24f96df79687c73ec2d291
Summit Shrestha <summits898@gmail.com>
2025-10-22 21:05:06 +0530

    calendar update

--- server-crm/index.js
+++ server-crm/index.js
@@ -48,10 +48,6 @@ app.use("/api/userProfile", userProfile);
 app.use("/api/forgetPass1", forgetPass1);
 app.use("/api/forgetPass2", forgetPass2);
 
-// import calendar from "./api/calendar/calendar.routes.js";
-// app.use("/api/events",calendar);
-
-
 import company from "./api/company/companie.routes.js";
 app.use("/api/registerComp", company);
 
